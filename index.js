d50fd7efc8227ace468dce82b66589f51f9e9898
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-11-01 17:56:28 -0400

    commit tree a5c429351a8e6f789de2a5c965bc063f549953e0

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
 
-// Write your code here
+
+let random = Math.round(Math.random() *10) + 1;
+
+let max = Math.max(1, 2, 20);
+
+let num3 = 18;
+let num4 = 7;
+let mod = (num3 % num4); 
